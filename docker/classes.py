c46a169d419abb1a87b5892d75af7eca12f5c922
camille <chuayuen0122@gmail.com>
2024-03-24 15:31:19 +0800

    Installed python-dotenv, updated requirements.txt and classes.py

--- docker/classes.py
+++ docker/classes.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from os import environ
+from dotenv import load_dotenv
+import os
 from sqlalchemy import func
 
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
